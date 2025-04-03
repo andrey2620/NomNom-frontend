baf7f602c54011d009d3c2bece3471f7dae6ea85
alisonapl <164436905+imalison@users.noreply.github.com>
2025-04-02 19:10:12 -0600

    Sous chef vos elegida

--- src/app/pages/recipe/sous-chef/sous-chef.component.ts
+++ src/app/pages/recipe/sous-chef/sous-chef.component.ts
@@ -16,6 +16,18 @@ export class SousChefComponent {
   public volume: number = 0.5;
   public rate: number = 1;
   public isSpeaking: boolean = false;
+  public isSousChefOn: boolean = false;
+  public selectedVoice: SpeechSynthesisVoice | null = null;
+
+  constructor() {
+    speechSynthesis.onvoiceschanged = () => {
+      const voices = speechSynthesis.getVoices();
+      this.selectedVoice = voices.find(v => v.lang === 'es-ES' && v.name.includes('Google español')) ||
+      voices.find(v => v.lang.startsWith('es')) ||
+      null;
+      console.log('Voz seleccionada:', this.selectedVoice?.name);
+    };
+  }
 
   speak() {
     if (!this.recipe) return;
@@ -25,6 +37,10 @@ export class SousChefComponent {
     utterance.volume = this.volume;
     utterance.rate = this.rate;
     utterance.lang = 'es-ES';
+
+    if (this.selectedVoice) {
+      utterance.voice = this.selectedVoice;
+    }
 
     speechSynthesis.speak(utterance);
     this.isSpeaking = true;
@@ -39,24 +55,26 @@ export class SousChefComponent {
     this.isSpeaking = false;
   }
 
-  private buildText(): string {
-    const intro = `Hola... Soy el sous chef de NomNom. Estoy en periodo de prueba y`;
-    const ingredientes = `Aun no puedo ayudarte a cocinar, lo siento.`;
-    const pasos = `Vuelve pronto`;
-    const sugerencias = `Aun no tengo sugerencias por compartir.`;
-    return `${intro} ${ingredientes} ${pasos}`;
-  }
-
-  public isSousChefOn: boolean = false;
-
   toggleSousChef() {
     this.isSousChefOn = !this.isSousChefOn;
-  
+
     if (this.isSousChefOn) {
-      this.speak(); //aun me falta poner el text to speack
+      this.speak();
     } else {
-      this.stop(); 
+      this.stop();
     }
   }
+
+  private buildText(): string {
+    const intro = `¡Bienvenido a la NomNomcocina! ... ¿Tienes tu delantal listo? Yo soy tu Sous Chef, Hoy tenemos una receta que te va a encantar. Vamos a preparar: ${this.recipe.name}. ¡Va a ser riquísimo!`;
   
+    const ingredientes = `Para empezar, vamos a necesitar estos ingredientes: ${this.recipe.instructions}. ¿Estás listo?`;
+  
+    const pasos = `Ahora, atención, porque vamos a hacer los pasos uno por uno: ${this.recipe.instructions}. ¡Buen trabajo, chef!`;
+  
+    const despedida = `Eso fue todo por hoy. ¡Nos vemos en la próxima receta!`;
+  
+    return `${intro} ${ingredientes} ${pasos} ${despedida}`;
+  }
 }
+
