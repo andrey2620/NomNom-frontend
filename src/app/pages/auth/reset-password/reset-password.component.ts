0acec5bb93d1032ab7f1a086615ed81337fd116b
Andrey <rvillalobosg@ucenfotec.ac.cr>
2025-04-04 04:50:20 -0600

    refactor layout and styling for profile and reset password components

--- src/app/pages/auth/reset-password/reset-password.component.ts
+++ src/app/pages/auth/reset-password/reset-password.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms'; // Importa FormsModule aquí
+import { FormsModule } from '@angular/forms';
 import { Router, ActivatedRoute } from '@angular/router';
 import { AuthService } from '../../../services/auth.service';
 import { ToastService } from '../../../services/toast.service';
@@ -8,13 +8,13 @@ import { ToastService } from '../../../services/toast.service';
 @Component({
   selector: 'app-reset-password',
   standalone: true,
-  imports: [CommonModule, FormsModule], // Agrega FormsModule aquí
+  imports: [CommonModule, FormsModule],
   templateUrl: './reset-password.component.html',
-  styleUrl: './reset-password.component.scss'
+  styleUrl: './reset-password.component.scss',
 })
 export class ResetPasswordComponent {
   public resetForm = { password: '', confirmPassword: '' };
-  private token: string = '';
+  private token = '';
 
   constructor(
     private route: ActivatedRoute,
@@ -23,17 +23,16 @@ export class ResetPasswordComponent {
     private toastService: ToastService
   ) {
     this.token = this.route.snapshot.queryParams['token'];
-    if(!this.token){
+    if (!this.token) {
       this.router.navigate(['/access-denied']);
     }
   }
 
   handleResetPassword(event: Event) {
     event.preventDefault();
-    
 
     const { password, confirmPassword } = this.resetForm;
-        
+
     if (!password || !confirmPassword) {
       this.toastService.showError('Todos los campos son obligatorios.');
       return;
@@ -49,12 +48,12 @@ export class ResetPasswordComponent {
         this.toastService.showSuccess('Cambio de contraseña exitoso. Ahora puedes iniciar sesión.');
         this.router.navigate(['/login']);
       },
-      error: (error) => {
-        this.toastService.showError(error.error?.message + ". Redirigiendo al login..." || 'Error cambiando contraseña.');
+      error: error => {
+        this.toastService.showError(error.error?.message + '. Redirigiendo al login...' || 'Error cambiando contraseña.');
         setTimeout(() => {
           this.router.navigateByUrl('login');
         }, 3000);
-      }
+      },
     });
   }
   navigateToLogin() {
