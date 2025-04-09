9104a075bdb54426070efc7805ca3a2305856159
Andrey <105994468+andrey2620@users.noreply.github.com>
2025-04-09 15:45:22 -0600

    fix google btn again and again

--- src/app/pages/auth/login/auth-config.ts
+++ src/app/pages/auth/login/auth-config.ts
@@ -5,10 +5,10 @@ export const authCodeFlowConfig: AuthConfig = {
   redirectUri: window.location.origin + '/callback',
   clientId: '648798311262-bk41l3qs500br4e0495eo69d3bfj7pj7.apps.googleusercontent.com',
   scope: 'openid profile email',
-  responseType: 'code',
+  responseType: 'id_token token',
   strictDiscoveryDocumentValidation: false,
   requireHttps: false,
   showDebugInformation: true,
   useHttpBasicAuth: false,
-  disablePKCE: false
+  disablePKCE: false,
 };
