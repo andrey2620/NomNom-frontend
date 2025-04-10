761f936b059fbf0bcb48a40fe6ddcd8f400cae3b
Gabriel Caravaca Garrido <gcaravacag@ucenfotec.ac.cr>
2025-04-09 21:33:22 -0600

    Update2. Chips funcionales
    Solo falta no permitir chips del mismo ingrediente

--- src/app/components/chip/chips.component.ts
+++ src/app/components/chip/chips.component.ts
@@ -6,15 +6,18 @@ import { FormsModule } from '@angular/forms';
   standalone: true,
   selector: 'app-chips',
   templateUrl: './chips.component.html',
+  styleUrls: ['./chips.component.scss'],
   imports: [
       CommonModule,
       FormsModule,
 
     ],
 })
+
 export class ChipsComponent {
   @Input() chips: string[] = [];
 
+  hoveredIndex: number | null = null;
   chipInput: string = '';
 
   addChip() {
