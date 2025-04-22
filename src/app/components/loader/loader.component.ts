0ceb622a2b6816a491821f868d94f798510c4e39
Andrey2620 <lopendex@gmail.com>
2025-04-22 01:53:38 -0600

    refactor(NomNomLandingPage): reorganize imports and improve code structure
    Restructure the NomNomLandingPage component by reorganizing imports, adding router functionality for smooth scrolling, and improving the overall code readability. The changes also include updates to the HTML and SCSS files for better maintainability and consistency.

--- src/app/components/loader/loader.component.ts
+++ src/app/components/loader/loader.component.ts
@@ -1,12 +1,12 @@
-import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-loader',
   standalone: true,
   imports: [CommonModule],
   templateUrl: './loader.component.html',
-  styleUrl: './loader.component.scss'
+  styleUrl: './loader.component.scss',
 })
 export class LoaderComponent {
   @Input() contrast: boolean = false;
