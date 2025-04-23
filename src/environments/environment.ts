6077416cbafe4d1b9d611556ec273bceeda1bb75
Andrey2620 <lopendex@gmail.com>
2025-04-22 22:26:27 -0600

    build: add @types/node dependency and update TypeScript config
    Add @types/node to the project dependencies and update the TypeScript configuration files to include "node" in the types array. This ensures proper type checking for Node.js APIs and aligns the project with the latest TypeScript standards. Also, update the environment.ts file to use process.env for dynamic API URL configuration based on the environment.

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,6 @@
-const apiUrls = 'https://andrey.tail29d229.ts.net';
+// Alternatively, you can use:
+const apiUrls = process.env['NODE_ENV'] === 'development' ? 'http://localhost:8080' : 'https://andrey.tail29d229.ts.net';
+
 export const environment = {
   production: false,
   apiUrl: apiUrls,
